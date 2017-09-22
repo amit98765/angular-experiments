d5054ffad519db6a2ef1b1dba83127e0a06305ad
amit98765 <amitkumarmittalakm@gmail.com>
2017-09-22 12:44:46 +1000

    two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,12 @@ export class AppComponent {
   title = 'app';
   name="A Big Name that could be seen";
   isZipCodeValid: boolean = true;
+  shippingAddress: string;
 
   changeName():void{
     this.name = "This is the changed name";
+  }
+  setDefaultSA():void{
+    this.shippingAddress = "123 ABC Road";
   }
 }
